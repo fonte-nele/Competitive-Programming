5c0f320a0f6d70c669168f3c04906cc39d0965dc
fonte-nele <felipe.fontenele.op@gmail.com>
2020-06-19 15:33:55 -0300

    #feat: add problemG.cpp Bubble Cup

---
+++ Bubble Cup 2º Replay Contest/problemG.cpp
@@ -0,0 +1,48 @@
+/*input
+3
+*/
+
+// Problem G - Rainha
+#include <bits/stdc++.h>
+using namespace std;
+
+#define f(inicio, fim) for(int i = inicio; i < fim; i++)
+#define fr(inicio, fim) for(int j = inicio; j < fim; j++)
+#define all(x) x.begin (), x.end ()
+#define sz(x) (int) x.size ()
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define se second
+#define eps 1e-9
+#define mem(x, val) memset ((x), (val), sizeof (x))
+#define LSONE(s) ((s)&(-s))
+#define INF 0x3f3f3f3f
+#define pi 3.1415926535897
+#define MAX 10005
+#define endl "\n"
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef string st;
+typedef vector <string> vs;
+typedef pair <int, int> ii;
+typedef pair <double, ii > iii;
+typedef vector <int> vi;
+typedef vector <ii> vii;
+typedef map <int, int> mii;
+
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    double m;
+
+    cin >> n;
+
+    m = (10*n - 12 + (2.0/n))/3; // Fórmula 
+    cout << fixed << setprecision(3) << m << endl;
+
+    return 0;
+}
